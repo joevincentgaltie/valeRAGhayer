b5437119ab70f006350df6358f989976f0ac38ae
VINCENT GALTIE Joe <jvincent-galtie-adc@solano.alize>
2024-03-21 18:21:21 +0100

    modif pour embedding de l'ensemble de la base

--- rag/create_embeddings.py
+++ rag/create_embeddings.py
@@ -6,15 +6,19 @@ from langchain.embeddings import HuggingFaceEmbeddings
 from langchain.vectorstores import FAISS
 
 
-loader = CSVLoader('../data/VALERIE_HAYER.csv', metadata_columns=["source","source_date"],encoding='utf-8')
+loader = CSVLoader('../data/all_french_explanations.csv',metadata_columns=["party", "number","name", "source", "source_date","orientation"], encoding="utf-8")
 documents =loader.load()
 
+
+documents = [doc for doc in documents if doc.metadata["party"] == "Groupe Renew Europe"]
+
 #CHunking the documents with Recursive Character Text Splitter
-splitter = RecursiveCharacterTextSplitter(chunk_size=512, chunk_overlap=30)
+splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=100)
 chunked_docs = splitter.split_documents(documents)
 
 embedding_function = HuggingFaceEmbeddings(model_name="sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2")
 
 db = FAISS.from_documents(chunked_docs, embedding_function)
-db.save_local("../data/faiss_index")
+db.save_local("../data/faiss_index_groupe_renew_europe")
+
 
