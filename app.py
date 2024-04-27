fc8b2ad01359e24d5273446c9419f105411124c0
joevincentgaltie <joevincentgaltie@gmail.com>
2024-04-27 18:24:49 +0200

    ajout pysqlite3

--- app.py
+++ app.py
@@ -1,3 +1,8 @@
+__import__('pysqlite3')
+import sys
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+
+
 import streamlit as st
 from langchain.document_loaders import CSVLoader
 from langchain_mistralai.chat_models import ChatMistralAI
