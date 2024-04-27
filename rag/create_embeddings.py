6f2f4b6f779741bcd67f8de025e1b448afbe5dbe
joevincentgaltie <joevincentgaltie@gmail.com>
2024-04-27 17:31:01 +0200

    modif version bonne pour runner

--- rag/create_embeddings.py
+++ rag/create_embeddings.py
@@ -5,22 +5,22 @@ from langchain_chroma import Chroma
 #import faiss
 
 from dotenv import load_dotenv
-import os 
+import os
 load_dotenv()
-API_MISTRAL = os.getenv("API_MISTRAL")
-
+api_key = os.getenv("API_MISTRAL")
+print(api_key)
 
 loader = CSVLoader('../data/all_french_explanations.csv',metadata_columns=["party", "number","name", "source", "source_date","orientation"], encoding="utf-8")
 documents =loader.load()
+print(len(documents))
 
 
-documents = [doc for doc in documents if doc.metadata["party"] == "Groupe Renew Europe"]
 
 #CHunking the documents with Recursive Character Text Splitter
 splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=100)
 chunked_docs = splitter.split_documents(documents)
 
-embeddings = MistralAIEmbeddings(model="mistral-embed", mistral_api_key=API_MISTRAL)
-db = Chroma.from_documents(documents, embeddings, persist_directory="../chroma_db", collection_name="Explanations")
+embeddings = MistralAIEmbeddings(model="mistral-embed", mistral_api_key=api_key)
+db = Chroma.from_documents(chunked_docs, embeddings, persist_directory="../chroma_db")
 
 
