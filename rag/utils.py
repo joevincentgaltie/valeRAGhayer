8ef14f0bad3cbcbc92d55e29fd12a34cda407364
VINCENT GALTIE Joe <jvincent-galtie-adc@solano.alize>
2024-05-02 19:41:22 +0200

    modif labels partis

--- rag/utils.py
+++ rag/utils.py
@@ -1,10 +1,10 @@
-mapper_partis = {'Groupe Renew Europe (Renaissance, MoDem, Parti radical, Horizons, Ecologie au centre) ': 'Groupe Renew Europe',
- "Groupe de l'Alliance Progressiste des Socialistes et Démocrates au Parlement européen (Parti Socialiste) ": "Groupe de l'Alliance Progressiste des Socialistes et Démocrates au Parlement européen",
- 'Groupe des Conservateurs et Réformistes européens (Reconquête) ': 'Groupe des Conservateurs et Réformistes européens',
- 'Groupe des Verts/Alliance libre européenne (EELV, Femu a Corsica, Union Démocratique Bretonne) ': 'Groupe des Verts/Alliance libre européenne',
- 'Groupe du Parti populaire européen (Démocrates-Chrétiens) (Les Républicains, les Centristes) ': 'Groupe du Parti populaire européen (Démocrates-Chrétiens)',
- 'Groupe «Identité et démocratie» (Rassemblement National, La Droite Populaire ) ': 'Groupe «Identité et démocratie»',
- 'Le groupe de la gauche au Parlement européen - GUE/NGL (LFI, Gauche républicaine et socialiste) ': 'Le groupe de la gauche au Parlement européen - GUE/NGL',
+mapper_partis = {'Renaissance, MoDem, Parti radical, Horizons & Ecologie au centre (Groupe Renew Europe) ': 'Groupe Renew Europe',
+ "Parti Socialiste (Groupe de l'Alliance Progressiste des Socialistes et Démocrates au Parlement européen)": "Groupe de l'Alliance Progressiste des Socialistes et Démocrates au Parlement européen",
+ 'Reconquête (Groupe des Conservateurs et Réformistes européens) ': 'Groupe des Conservateurs et Réformistes européens',
+ 'EELV, Femu a Corsica & Union Démocratique Bretonne  (Groupe des Verts/Alliance libre européenne)': 'Groupe des Verts/Alliance libre européenne',
+ 'Les Républicains & les Centristes (Groupe du Parti populaire européen / Démocrates-Chrétiens)  ': 'Groupe du Parti populaire européen (Démocrates-Chrétiens)',
+ 'Rassemblement National & La Droite Populaire (Groupe «Identité et démocratie»)': 'Groupe «Identité et démocratie»',
+ 'LFI & Gauche républicaine et socialiste (Le groupe de la gauche au Parlement européen - GUE/NGL) ': 'Le groupe de la gauche au Parlement européen - GUE/NGL',
  }
 
 #'Non-inscrits (Non-inscrits) ': 'Non-inscrits'
